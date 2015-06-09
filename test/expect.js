5063ab108fb096d57f3b95f06335a3e91b63bb03
Ricardo Barros <ricardofbarros@hotmail.com>
2015-06-09 16:46:15 +0100

    add expect API test

---
+++ test/expect.js
@@ -0,0 +1,109 @@
+/* global describe, it */
+
+// Dependencies
+var Examine = require('../index')
+var async = require('async')
+var chai = require('chai')
+var AssertionError = chai.AssertionError
+
+// Build a examine
+var examine = new Examine()
+var expect = examine.expect
+
+// Helper to test throws
+function testThrow (fn, cb) {
+  try {
+    fn()
+  } catch(err) {
+    if (!(err instanceof AssertionError)) {
+      return cb(new Error('error is not from AssertionError, could be syntax error'))
+    }
+
+    if (!(/prefix testing/.test(err.message))) {
+      return cb(new Error('error message wasn\'t passed properly'))
+    }
+
+    return cb()
+  }
+
+  return cb(new Error('didn\'t thrown an error'))
+}
+
+function testChain (apiChain) {
+  if (typeof apiChain === 'undefined') {
+    throw new Error('Invalid expect chain')
+  }
+}
+
+describe('examine.expect # full API test', function () {
+  describe('expect chains', function () {
+    // Expect chains:
+    var chains = [
+      'to',
+      'be',
+      'been',
+      'is',
+      'that',
+      'which',
+      'and',
+      'has',
+      'have',
+      'with',
+      'at',
+      'of',
+      'same'
+    ]
+
+    var s = 'hey'
+
+    // Generate tests for all chains
+    // lazy way ftw!
+    chains.forEach(function (describeChain) {
+      describe('expect.' + describeChain, function () {
+        it('should not true an error', function () {
+          chains.forEach(function (chain) {
+            testChain(expect(s)[describeChain][chain])
+          })
+        })
+      })
+    })
+  })
+
+  describe('expect.not', function () {
+    var foo = 'foo'
+    var bar = 'bar'
+    var goodFn = function () { return 'happy!' }
+    var badFn = function () { throw new Error('bad function!') }
+    var obj = { foo: 'baz' }
+
+    it('should assert successfuly', function () {
+      expect(foo).to.not.equal('bar')
+      expect(goodFn).to.not.throw(Error)
+      expect(obj).to.have.property('foo')
+        .and.not.equal('bar')
+    })
+
+    it('should throw an error', function (next) {
+      // Tests that should fail
+      var tests = {
+        '1': function () {
+          expect(bar, 'prefix testing').to.not.equal('bar')
+        },
+        '2': function () {
+          expect(badFn, 'prefix testing').to.not.throw(Error)
+        },
+        '3': function () {
+          expect(obj, 'prefix testing').to.have.property('foo')
+            .and.not.equal('baz')
+        }
+      }
+
+      // Run failing tests
+      return async.waterfall([
+        testThrow.bind(null, tests['1']),
+        testThrow.bind(null, tests['2']),
+        testThrow.bind(null, tests['3'])
+      ], next)
+    })
+  })
+})
